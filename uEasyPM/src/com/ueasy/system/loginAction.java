ef5604935e36fa15870f8705392c71be1a691b52
helen.zhou <zfayez.z@gmail.com>
2013-05-13 22:36:07 +0800

    add login

---
+++ uEasyPM/src/com/ueasy/system/loginAction.java
@@ -0,0 +1,10 @@
+package com.ueasy.system;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class loginAction extends ActionSupport {
+	public String execute() throws Exception
+    {
+		return "";
+    }
+}
