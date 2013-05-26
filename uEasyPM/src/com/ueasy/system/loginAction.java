a21e4133351f7cdee252b2e61e43fddc97fae8d8
helen.zhou <zfayez.z@gmail.com>
2013-05-26 11:10:41 +0800

    add  dojo

--- uEasyPM/src/com/ueasy/system/loginAction.java
+++ uEasyPM/src/com/ueasy/system/loginAction.java
@@ -3,8 +3,24 @@ package com.ueasy.system;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class loginAction extends ActionSupport {
+	private String user;
+	
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	//登陆路径为：http://localhost:8080/struts2/ueasy/login.action
 	public String execute() throws Exception
     {
 		return "";
     }
+	 public  void  validate(){
+         //效验内容
+        if(user==null){
+        	System.out.println("please login in !");
+        }
+	 }
 }
